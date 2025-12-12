0979a8cd8d1eecf2508def27ae81c9eb42a5035c
Jeffrey Goett <jgoett2@yahoo.com>
2025-12-12 17:09:07 -0500

    Added additional cycles

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 st.title("Jeff")
 
-for scenario in range(0,2**18):
+for scenario in range(0,2**23):
   a = np.uint32(14)
   b = np.uint32(15)
   x = np.uint32(~(a^b))
@@ -12,4 +12,4 @@ for scenario in range(0,2**18):
     total += x%2
     x = x//2
 print("Finished!")
-st.title("Dave")
+st.title("Dave!")
