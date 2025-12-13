1cd97ffdf62aa001d23da73d18f856dedffc212e
Jeffrey Goett <jgoett2@yahoo.com>
2025-12-13 05:38:09 -0500

    Test np array

--- test.py
+++ test.py
@@ -3,13 +3,29 @@ import streamlit as st
 
 st.title("Jeff")
 
-for scenario in range(0,2**23):
-  a = np.uint32(14)
-  b = np.uint32(15)
-  x = np.uint32(~(a^b))
+
+x = np.array([[1,np.uint32(4)], 
+              [2,np.uint32(15)],
+              [3,np.uint32(15)],
+              [4,np.uint32(15)],
+              [5,np.uint32(15)],
+              [6,np.uint32(15)],
+              [7,np.uint32(15)],
+              [8,np.uint32(15)],
+              [9,np.uint32(15)],
+              [10,np.uint32(15)],
+              [11,np.uint32(15)],
+              [12,np.uint32(15)],
+              ])
+
+for scenario in range(0,2**18):
+  a = ~(x[:,1]^4)
   total = 0
   for i in range(0,18):
-    total += x%2
-    x = x//2
+    total += a%2
+    a = a//2
+  
+
+
 print("Finished!")
 st.title("Dave!")
